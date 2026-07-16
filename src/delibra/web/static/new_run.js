08c4c84944524a956d3a47d0d8d381638d61af59
gotoo <gautier.duval@gmail.com>
2026-07-16 17:07:26 +0200

    Hide model field when provider does not require one

--- src/delibra/web/static/new_run.js
+++ src/delibra/web/static/new_run.js
@@ -1,11 +1,11 @@
 (function () {
   const presetSelect = document.querySelector('[data-preset-select]');
   const presetDetails = Array.from(document.querySelectorAll('[data-preset-detail]'));
-  if (!(presetSelect instanceof HTMLSelectElement) || presetDetails.length === 0) {
-    return;
-  }
 
   function syncPresetDetail() {
+    if (!(presetSelect instanceof HTMLSelectElement)) {
+      return;
+    }
     const selected = presetSelect.value;
     for (const detail of presetDetails) {
       if (!(detail instanceof HTMLDetailsElement)) {
@@ -16,6 +16,29 @@
     }
   }
 
-  presetSelect.addEventListener("change", syncPresetDetail);
-  syncPresetDetail();
+  if (presetDetails.length > 0) {
+    presetSelect?.addEventListener("change", syncPresetDetail);
+    syncPresetDetail();
+  }
+})();
+
+(function () {
+  const providerSelect = document.querySelector('[data-provider-select]');
+  const modelField = document.querySelector('[data-model-field]');
+  const modelInput = modelField?.querySelector('input[name="model"]');
+  if (!(providerSelect instanceof HTMLSelectElement) || !(modelInput instanceof HTMLInputElement)) {
+    return;
+  }
+
+  function syncModelField() {
+    const selected = providerSelect.selectedOptions[0];
+    const modelRequired = selected?.dataset.modelRequired === "true";
+    if (modelField instanceof HTMLElement) {
+      modelField.hidden = !modelRequired;
+    }
+    modelInput.disabled = !modelRequired;
+  }
+
+  providerSelect.addEventListener("change", syncModelField);
+  syncModelField();
 })();
